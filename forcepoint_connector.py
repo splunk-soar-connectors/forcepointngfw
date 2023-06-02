ff4973679f1f0d985461af5c44cef5b6e62dbfbe
nikhileshcr-splunk <nchaudhari@splunk.com>
2023-06-02 12:16:29 +1000

    python version upgrade

--- forcepoint_connector.py
+++ forcepoint_connector.py
@@ -1,12 +1,12 @@
 # Phantom App imports
-import phantom.app as phantom
-from phantom.base_connector import BaseConnector
-from phantom.action_result import ActionResult
+import json
 
+import phantom.app as phantom
 # Usage of the consts file is recommended
 # from forcepoint_consts import *
 import requests
-import json
+from phantom.action_result import ActionResult
+from phantom.base_connector import BaseConnector
 
 
 class RetVal(tuple):
@@ -184,11 +184,12 @@ class ForcepointConnector(BaseConnector):
 if __name__ == '__main__':
 
     import sys
+
     import pudb
     pudb.set_trace()
 
     if (len(sys.argv) < 2):
-        print "No test json specified as input"
+        print("No test json specified as input")
         exit(0)
 
     with open(sys.argv[1]) as f:
@@ -199,6 +200,6 @@ if __name__ == '__main__':
         connector = ForcepointConnector()
         connector.print_progress_message = True
         ret_val = connector._handle_action(json.dumps(in_json), None)
-        print (json.dumps(json.loads(ret_val), indent=4))
+        print(json.dumps(json.loads(ret_val), indent=4))
 
     exit(0)
--- forcepoint_consts.py
+++
@@ -1,4 +0,0 @@
-FORCEPOINT_URL = "base_url"
-FORCEPOINT_PORT = "base_port"
-FORCEPOINT_VERSION = "base_version"
-FORCEPOINT_AUTH_KEY = "auth_key"

9f7ea0218c83f71df1fec8313d1e4a9b60917949
Scott Odle <sodle@splunk.com>
2026-07-17 22:45:06 -0600

    Secure Forcepoint SMC API transport

--- forcepoint_connector.py
+++ forcepoint_connector.py
@@ -45,9 +45,9 @@ class ForcepointConnector(BaseConnector):
         self._force_port = config.get("base_port")
         self._force_version = config.get("base_version")
         self._force_auth_key = config.get("auth_key")
-        self._verify = config.get(phantom.APP_JSON_VERIFY, False)
+        self._verify = config.get(phantom.APP_JSON_VERIFY, True)
 
-        self.url = "http://" + self._force_url + ":" + self._force_port + "/" + self._force_version
+        self.url = "https://" + self._force_url + ":" + self._force_port + "/" + self._force_version
 
         session = requests.session()
 
